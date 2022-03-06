3441f3f511bc07347d07dbe87431cbfcc5b41db4
Chase Lee <72054052+Chase-Lee-ui@users.noreply.github.com>
2022-03-06 12:32:53 -1000

    bonk

--- 7drl-game-jam/Assets/Scripts/Camera_Bounds.cs
+++ 7drl-game-jam/Assets/Scripts/Camera_Bounds.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Camera_Bounds : MonoBehaviour
 {
     public float SmoothFactor;
+    public GameObject Player;
 
     private void FixedUpdate()
     {
@@ -13,7 +14,8 @@ public class Camera_Bounds : MonoBehaviour
 
     void Follow()
     {
-        Vector3 smoothPosition = Vector3.Lerp(transform.position, this.gameObject.transform.position, SmoothFactor*Time.fixedDeltaTime);
+        Vector3 PlayerOffset = new Vector3(this.Player.transform.position.x, this.Player.transform.position.y, -5);
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, PlayerOffset, SmoothFactor*Time.fixedDeltaTime);
         transform.position = smoothPosition;
     }
 }
