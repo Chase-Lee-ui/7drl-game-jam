05313de4802bba3ad767b1e1095896b6b7ce53d4
JustinY <jyip808@hawaii.edu>
2022-03-11 13:45:13 -1000

    combo

---
+++ 7drl-game-jam/Assets/Scripts/Combo_Manager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combo_Manager : MonoBehaviour
+{
+    //The current count in the combo
+    public int comboCount;
+    //Time left remaining in combo
+    public float timeLeft;
+    //Time combo last
+    public int comboTime;
+    public Enemy enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = 0;
+        comboCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft = timeLeft - Time.deltaTime;
+        if (enemy.Health == 0) 
+        {
+            comboCount++;
+            timeLeft = comboTime;
+        }
+
+        if (timeLeft == 0) 
+        {
+            comboCount = 0;
+        }
+    }
+}
