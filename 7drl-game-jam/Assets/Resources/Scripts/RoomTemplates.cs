347749b86416a64720dafa0aa76f75a46bec2789
Chase Lee <72054052+Chase-Lee-ui@users.noreply.github.com>
2022-03-11 18:37:00 -1000

    pushies

--- 7drl-game-jam/Assets/Resources/Scripts/RoomTemplates.cs
+++ 7drl-game-jam/Assets/Resources/Scripts/RoomTemplates.cs
@@ -16,8 +16,32 @@ public class RoomTemplates : MonoBehaviour {
 	public float waitTime;
 	private bool spawnedBoss;
 	public GameObject boss;
+	public int LimitRooms;
+
+	void Start()
+	{
+		
+	}
 
 	void Update(){
+		if(rooms.Count >= LimitRooms)
+		{
+			var saveRoomB = bottomRooms[0];
+			bottomRooms = new GameObject[1];
+			bottomRooms[0] = saveRoomB;
+
+			var saveRoomT = topRooms[0];
+			topRooms = new GameObject[1];
+			topRooms[0] = saveRoomT;
+
+			var saveRoomR = rightRooms[0];
+			rightRooms = new GameObject[1];
+			rightRooms[0] = saveRoomR;
+
+			var saveRoomL = leftRooms[0];
+			leftRooms = new GameObject[1];
+			leftRooms[0] = saveRoomL;
+		}
 
 		if(waitTime <= 0 && spawnedBoss == false){
 			for (int i = 0; i < rooms.Count; i++) {
